1d380b8ac1be446dba99363e32f74b34216ae013
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-02-18 23:35:05 +0530

    Activity LifeCycle (#1)
    * renaming of activities
    
    * renaming of activities
    
    * New activity ie SecondActivity.kt added
    
    * Added view binding for activity layouts
    
    * Override lifeycle methods in FirstActivity.kt
    
    * Override lifeycle methods in SecondActivity.kt
    
    * logs added to activity lifecycle callbacks
    
    * navigation added for first to second activity

---
+++ app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
@@ -0,0 +1,74 @@
+package com.example.dissectinglifecycle
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.dissectinglifecycle.databinding.ActivityFirstBinding
+
+class FirstActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "FirstActivity"
+    }
+
+    private val binding : ActivityFirstBinding by lazy {
+        ActivityFirstBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setClickListeners()
+        Log.d(TAG, "onCreate")
+    }
+
+    private fun setClickListeners() {
+        with(binding) {
+            btnGoToSecondActivity.setOnClickListener {
+                startActivity(Intent(this@FirstActivity, SecondActivity::class.java))
+            }
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+}
