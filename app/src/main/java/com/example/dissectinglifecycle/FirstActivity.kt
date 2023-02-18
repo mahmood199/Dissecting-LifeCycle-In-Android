b7eab758021bab2535f2412eab87fc74ba24d3a7
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-18 23:27:31 +0530

    Override lifeycle methods in FirstActivity.kt

--- app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
+++ app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
@@ -9,8 +9,42 @@ class FirstActivity : AppCompatActivity() {
     private val binding : ActivityFirstBinding by lazy {
         ActivityFirstBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
