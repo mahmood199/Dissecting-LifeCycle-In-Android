947eb73b2c734edf66391ebcd827ff26a52d07b1
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-18 23:32:44 +0530

    logs added to activity lifecycle callbacks

--- app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
+++ app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
@@ -2,9 +2,14 @@ package com.example.dissectinglifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.dissectinglifecycle.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "FirstActivity"
+    }
 
     private val binding : ActivityFirstBinding by lazy {
         ActivityFirstBinding.inflate(layoutInflater)
@@ -13,38 +18,47 @@ class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        Log.d(TAG, "onCreate")
     }
 
     override fun onRestart() {
         super.onRestart()
+        Log.d(TAG, "onRestart")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d(TAG, "onStart")
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d(TAG, "onResume")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d(TAG, "onPause")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
     }
 
     override fun onStop() {
         super.onStop()
+        Log.d(TAG, "onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
 
 }
--- app/src/main/java/com/example/dissectinglifecycle/SecondActivity.kt
+++ app/src/main/java/com/example/dissectinglifecycle/SecondActivity.kt
@@ -2,10 +2,15 @@ package com.example.dissectinglifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.dissectinglifecycle.databinding.ActivityFirstBinding
 import com.example.dissectinglifecycle.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "SecondActivity"
+    }
 
     private val binding : ActivitySecondBinding by lazy {
         ActivitySecondBinding.inflate(layoutInflater)
@@ -18,34 +23,41 @@ class SecondActivity : AppCompatActivity() {
 
     override fun onRestart() {
         super.onRestart()
+        Log.d(TAG, "onRestart")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d(TAG, "onStart")
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.d(TAG, "onResume")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d(TAG, "onPause")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState")
     }
 
     override fun onStop() {
         super.onStop()
+        Log.d(TAG, "onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d(TAG, "onDestroy")
     }
-
 }
