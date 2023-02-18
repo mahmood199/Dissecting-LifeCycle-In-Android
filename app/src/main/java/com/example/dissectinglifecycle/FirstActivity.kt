d075d572d2e877e4cd01bfa7eab300c5f41bfec6
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-18 23:22:26 +0530

    Added view binding for activity layouts

--- app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
+++ app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
@@ -2,10 +2,15 @@ package com.example.dissectinglifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dissectinglifecycle.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
+
+    private val binding : ActivityFirstBinding by lazy {
+        ActivityFirstBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/dissectinglifecycle/SecondActivity.kt
+++ app/src/main/java/com/example/dissectinglifecycle/SecondActivity.kt
@@ -2,10 +2,18 @@ package com.example.dissectinglifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dissectinglifecycle.databinding.ActivityFirstBinding
+import com.example.dissectinglifecycle.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private val binding : ActivitySecondBinding by lazy {
+        ActivitySecondBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(binding.root)
+
     }
 }
