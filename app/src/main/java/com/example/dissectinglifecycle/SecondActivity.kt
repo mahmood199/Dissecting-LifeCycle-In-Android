5356e17a069d3e4934ed776308a9bc7a4e0e1c60
MSI KATANA <mohammed.mahmood@semaai.com>
2023-02-18 23:27:40 +0530

    Override lifeycle methods in SecondActivity.kt

--- app/src/main/java/com/example/dissectinglifecycle/SecondActivity.kt
+++ app/src/main/java/com/example/dissectinglifecycle/SecondActivity.kt
@@ -14,6 +14,38 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
     }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
