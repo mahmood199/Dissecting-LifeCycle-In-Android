ec02657f9d4774bad456b01f0c6e292d446c98d1
Mohammed Mahmood Ahmed <gaudentas199@gmail.com>
2023-02-23 17:26:57 +0530

    Logs added for no backstack and with backstack (#8)
    * Logs added for no backstack and with backstack
    
    * Logs and click listener added for replace fragment transactions
    
    * Update README.md

--- app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
+++ app/src/main/java/com/example/dissectinglifecycle/FirstActivity.kt
@@ -1,6 +1,5 @@
 package com.example.dissectinglifecycle
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -29,34 +28,70 @@ class FirstActivity : AppCompatActivity() {
 
     private fun setClickListeners() {
         with(binding) {
-            btn1.setOnClickListener {
-                Log.d(TAG, "Any action that tries to add fragment 1")
-                launchFirstFragment(true)
+            addFirstWithoutBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to add fragment 1 without backstack")
+                addFirstFragment(false)
             }
 
-            btn2.setOnClickListener {
-                Log.d(TAG, "Any action that tries to add fragment 2")
-                launchSecondFragment(true)
+            addSecondWithoutBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to add fragment 2 without backstack")
+                addSecondFragment(false)
             }
 
-            btn3.setOnClickListener {
-                Log.d(TAG, "Any action that tries to add fragment 3")
-                launchThirdFragment(true)
+            addThirdWithoutBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to add fragment 3 without backstack")
+                addThirdFragment(false)
             }
 
-            btn4.setOnClickListener {
-                Log.d(TAG, "Any action that tries to go to next activity")
-                Log.d(TAG, "Any action that tries to add fragment")
-                fragmentTransactionDelegate.addFragment(
-                    R.id.fl_fragment_container,
-                    Fragment1(),
-                    Fragment1.TAG
-                )
+            addFirstWithBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to add fragment 1 with backstack")
+                addFirstFragment(true)
             }
+
+            addSecondWithBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to add fragment 2 with backstack")
+                addSecondFragment(true)
+            }
+
+            addThirdWithBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to add fragment 3 with backstack")
+                addThirdFragment(true)
+            }
+
+            replaceFirstWithoutBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to replace fragment 1 without backstack")
+                replaceWithFirstFragment(false)
+            }
+
+            replaceSecondWithoutBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to replace fragment 2 without backstack")
+                replaceWithSecondFragment(false)
+            }
+
+            replaceThirdWithoutBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to replace fragment 3 without backstack")
+                replaceWithThirdFragment(false)
+            }
+
+            replaceFirstWithBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to replace fragment 1 with backstack")
+                replaceWithFirstFragment(true)
+            }
+
+            replaceSecondWithBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to replace fragment 2 with backstack")
+                replaceWithSecondFragment(true)
+            }
+
+            replaceThirdWithBackstack.setOnClickListener {
+                Log.d(TAG, "Any action that tries to replace fragment 3 with backstack")
+                replaceWithThirdFragment(true)
+            }
+
         }
     }
 
-    private fun launchFirstFragment(withBackStack: Boolean) {
+    private fun addFirstFragment(withBackStack: Boolean) {
         if (withBackStack)
             fragmentTransactionDelegate.addFragmentWithBackStack(
                 R.id.fl_fragment_container,
@@ -71,7 +106,7 @@ class FirstActivity : AppCompatActivity() {
             )
     }
 
-    private fun launchSecondFragment(withBackStack: Boolean) {
+    private fun addSecondFragment(withBackStack: Boolean) {
         if (withBackStack)
             fragmentTransactionDelegate.addFragmentWithBackStack(
                 R.id.fl_fragment_container,
@@ -86,7 +121,7 @@ class FirstActivity : AppCompatActivity() {
             )
     }
 
-    private fun launchThirdFragment(withBackStack: Boolean) {
+    private fun addThirdFragment(withBackStack: Boolean) {
         if (withBackStack)
             fragmentTransactionDelegate.addFragmentWithBackStack(
                 R.id.fl_fragment_container,
@@ -100,6 +135,49 @@ class FirstActivity : AppCompatActivity() {
                 Fragment3.TAG
             )
     }
+
+    private fun replaceWithFirstFragment(withBackStack: Boolean) {
+        if(withBackStack)
+            fragmentTransactionDelegate.replaceFragment(
+                R.id.fl_fragment_container,
+                Fragment1(),
+                Fragment1.TAG
+            )
+        else
+            fragmentTransactionDelegate.replaceFragmentWithoutBackStack(
+                R.id.fl_fragment_container,
+                Fragment1(),
+            )
+    }
+
+    private fun replaceWithSecondFragment(withBackStack: Boolean) {
+        if(withBackStack)
+            fragmentTransactionDelegate.replaceFragment(
+                R.id.fl_fragment_container,
+                Fragment2(),
+                Fragment2.TAG
+            )
+        else
+            fragmentTransactionDelegate.replaceFragmentWithoutBackStack(
+                R.id.fl_fragment_container,
+                Fragment2(),
+            )
+    }
+
+    private fun replaceWithThirdFragment(withBackStack: Boolean) {
+        if(withBackStack)
+            fragmentTransactionDelegate.replaceFragment(
+                R.id.fl_fragment_container,
+                Fragment3(),
+                Fragment3.TAG
+            )
+        else
+            fragmentTransactionDelegate.replaceFragmentWithoutBackStack(
+                R.id.fl_fragment_container,
+                Fragment3(),
+            )
+    }
+
 
     override fun onRestart() {
         super.onRestart()
